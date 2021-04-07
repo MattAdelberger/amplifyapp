5a024042804b637de006bb5c2e10cea6c192a9d6
matt.adelberger@gmail.com <matt.adelberger@gmail.com>
2021-04-06 21:19:01 -0500

    Second Kit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          My Changes work in AWS
         </p>
         <a
           className="App-link"
